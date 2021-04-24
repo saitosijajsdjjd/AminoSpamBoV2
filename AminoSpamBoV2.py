90b067358d3186a553f7fe62b6c503ccf4c968e7
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-04-24 16:51:22 +0800

    Update AminoSpamBoV2.py

--- AminoSpamBoV2.py
+++ AminoSpamBoV2.py
@@ -1,7 +1,4 @@
-from concurrent.futures import ThreadPoolExecutor
-import concurrent.futures
-from colorama import init
-from colorama import Fore, Back, Style
+from colorama import init, Fore, Back, Style
 init()
 print(Back.BLACK)
 print(Fore.CYAN)
@@ -18,9 +15,7 @@ print("▀░▀ ▀░░░▀ ▀ ▀░░▀ ░▀░ ▀▀░ █▀░ 
 print("▐▌░▐▌ █▀▀ █▀▀▄ ▄▀▀ ▀ ▄▀▄ █▄░█ ▒▄▀▄")
 print("░▀▄▀░ █▀▀ █▐█▀ ░▀▄ █ █░█ █░▀█ ░▒▄▀")
 print("░░▀░░ ▀▀▀ ▀░▀▀ ▀▀░ ▀ ░▀░ ▀░░▀ ▒█▄▄")
-chatlist = []
-chatnames = {}
-import amino
+import amino, concurrent.futures
 email = input("Email/Почта: ")
 password = input("Password/Пароль: ")
 message = input("Message/Сообщение: ")
@@ -32,15 +27,10 @@ for x, name in enumerate(clients.name, 1):
     print(f"{x}.{name}")
 communityid = clients.comId[int(input("Выберите сообщество/Select the community: "))-1]
 sub_client = amino.SubClient(comId=communityid, profile=client.profile)
-z=0
-chats = sub_client.get_chat_threads(size=1000)
-for name, id in zip (chats.title, chats.chatId):
-	if name!=None:
-		print(f"{z + 1}.{name}")  
-		chatnames[x]=str(name)
-		chatlist.append(str(id))
-		z+=1
-chatx = chatlist[int(input("Выберите чат/Select the chat: "))-1]
+chats = sub_client.get_chat_threads(size=150)
+for z, title in enumerate(chats.title, 1):
+	print(f"{z}.{title}")
+chatx = chats.chatId[int(input("Выберите чат/Select the chat: "))-1]
 
 sub_client.send_message(chatId=chatx, message=message, messageType=msgtype)
 
