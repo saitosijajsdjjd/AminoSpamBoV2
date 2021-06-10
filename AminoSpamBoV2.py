e73dff7d0ae681c47e9ef8b9c40b97259d8663fd
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-06-10 09:53:13 +0600

    Update AminoSpamBoV2.py

--- AminoSpamBoV2.py
+++ AminoSpamBoV2.py
@@ -1,16 +1,13 @@
+import amino
+import concurrent.futures
+import pyfiglet
 from colorama import init, Fore, Back, Style
 init()
 print(Back.BLACK)
 print(Fore.CYAN)
 print("""Script by Lil Zevi
-Github : https://github.com/LilZeviameBo
-▄▀▄ █▄░▄█ ▀ █▄░█ ▄▀▄ ▄▀▀ █▀▄ ▄▀▄ █▄░▄█ █▀▄ ▄▀▄
-█▀█ █░█░█ █ █░▀█ █░█ ░▀▄ █░█ █▀█ █░█░█ █▀█ █░█
-▀░▀ ▀░░░▀ ▀ ▀░░▀ ░▀░ ▀▀░ █▀░ ▀░▀ ▀░░░▀ ▀▀░ ░▀░
-▐▌░▐▌ █▀▀ █▀▀▄ ▄▀▀ ▀ ▄▀▄ █▄░█ ▒▄▀▄
-░▀▄▀░ █▀▀ █▐█▀ ░▀▄ █ █░█ █░▀█ ░▒▄▀
-░░▀░░ ▀▀▀ ▀░▀▀ ▀▀░ ▀ ░▀░ ▀░░▀ ▒█▄▄""")
-import amino, concurrent.futures
+Github : https://github.com/LilZevi""")
+print(pyfiglet.figlet_format("aminospambov2", font="drpepper"))
 email = input("Email/Почта: ")
 password = input("Password/Пароль: ")
 message = input("Message/Сообщение: ")
@@ -27,9 +24,15 @@ for z, title in enumerate(chats.title, 1):
 	print(f"{z}.{title}")
 chatx = chats.chatId[int(input("Выберите чат/Select the chat: "))-1]
 
-sub_client.send_message(chatId=chatx, message=message, messageType=msgtype)
-
-while True:
-	with concurrent.futures.ThreadPoolExecutor(max_workers=1000000) as executor:
-		_ = [executor.submit(sub_client.send_message, chatx, message, msgtype) for _ in range(5000000)]
-		
+def threadpoolspam():
+	while True:
+		with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
+			print("Spamming")
+			_ = [executor.submit(sub_client.send_message, chatx, message, msgtype) for _ in range(5000000)]
+			
+def threadpoolboost():
+	while True:
+		with concurrent.futures.ThreadPoolExecutor(max_workers=1000) as executor:
+			_ = [executor.submit(threadpoolspam) for _ in range(5000000)]
+			
+threadpoolboost()
